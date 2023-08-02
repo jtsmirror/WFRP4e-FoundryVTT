d1d37478c4e2a9250d6c93ff80b44ec3883bb088
jts mirror <radekbrzostek500@gmail.com>
2023-08-03 00:18:29 +0200

    Naprawienie Statusów

---
+++ modules/hooks/i18n.js
@@ -0,0 +1,14 @@
+
+
+export default function () {
+    Hooks.on("i18nInit", () => {
+        // Localize strings in the  game.wfrp4e.config.object
+        for (let obj of game.wfrp4e.config.toTranslate) {
+                for (let el in game.wfrp4e.config[obj]) {
+                    if (typeof game.wfrp4e.config[obj][el] === "string") {
+                        game.wfrp4e.config[obj][el] = game.i18n.localize(game.wfrp4e.config[obj][el])
+                    }
+                }
+        }
+    })
+}
